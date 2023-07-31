1e4479cad8e21436ed792ef28954964643f9d6aa
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-07-31 00:23:09 -0300

    Changed port.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import routers from './routers/indexRouter.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(routers);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Application running on port ${port}.`));
